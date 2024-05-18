220ee96e6cae19ceecf6f32ade203da3a46394a0
caregor <64469336+caregor@users.noreply.github.com>
2024-05-18 13:18:08 +0500

    HW 1 is done;

--- src/seminars/first/Calculator/CalculatorTest.java
+++ src/seminars/first/Calculator/CalculatorTest.java
@@ -73,5 +73,25 @@ public class CalculatorTest {
         // }
         //   assert 0 == seminars.first.Calculator.Calculator.calculation(2, 6, '+');
         //    assertThat(seminars.first.Calculator.Calculator.calculation(2, 6, '+')).isEqualTo(0);
+
+        //HW 1 проверки функции calculatingDiscount
+
+        // positive tests
+        assertThat(Calculator.calculatingDiscount(1, 10)).isEqualTo(0.9);
+        assertThat(Calculator.calculatingDiscount(100, 0)).isEqualTo(100);
+        assertThat(Calculator.calculatingDiscount(100, 1)).isEqualTo(99);
+        assertThat(Calculator.calculatingDiscount(1000, 50)).isEqualTo(500);
+        assertThat(Calculator.calculatingDiscount(1000, 99)).isEqualTo(10);
+        assertThat(Calculator.calculatingDiscount(100, 100)).isEqualTo(0);
+
+        //negative tests
+        assertThatThrownBy(() -> Calculator.calculatingDiscount(-1, 10))
+                .isInstanceOf(ArithmeticException.class);
+        assertThatThrownBy(() -> Calculator.calculatingDiscount(0, 10))
+                .isInstanceOf(ArithmeticException.class);
+        assertThatThrownBy(() -> Calculator.calculatingDiscount(1, -1))
+                .isInstanceOf(ArithmeticException.class);
+        assertThatThrownBy(() -> Calculator.calculatingDiscount(101, 101))
+                .isInstanceOf(ArithmeticException.class);
     }
 }
